8439389d789ae06d4449c13630421f0fa44c1f88
Alexander Kubyshev <akubyshev@gmail.com>
2023-09-13 13:57:14 +0300

    Homework Seminar #5

---
+++ task_1.js
@@ -0,0 +1,32 @@
+// Задание 1
+// Дан объект numbers. Необходимо в консоль вывести все значения больше или равные 3.
+
+// const numbers = {
+// keyin1: 1,
+// keyin2: 2,
+// keyin3: 3,
+// keyin4: 4,
+// keyin5: 5,
+// keyin6: 6,
+// keyin7: 7,
+// }
+
+const numbers = {
+  keyin1: 1,
+  keyin2: 2,
+  keyin3: 3,
+  keyin4: 4,
+  keyin5: 5,
+  keyin6: 6,
+  keyin7: 7,
+};
+
+for (key in numbers) {
+  if (numbers[key] >= 3) {
+    console.log(numbers[key]);
+  }
+}
+
+// Object.values(numbers).forEach((el) => {
+//   if (el >= 3) console.log(el);
+// });
